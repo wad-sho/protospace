babd06c82064e13c62edafc37f9f4b8616401167
shogo1007 <67449307+wad-sho@users.noreply.github.com>
2023-02-14 17:59:15 +0900

    Revert "userモデルに変更"
    This reverts commit e0a7ffd9e74fe0c7cb9946ddcce75498e2e69b57.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
- 
+  devise_for :wadas
   get 'prototypes/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
